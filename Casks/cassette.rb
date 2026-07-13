04381ee28d2ffa760089dc55f6901930767815ff
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-07-13 20:01:09 +0200

    chore(cask): point release URLs to CassetteLab org

--- Casks/cassette.rb
+++ Casks/cassette.rb
@@ -2,11 +2,17 @@ cask "cassette" do
   version "1.8.4"
   sha256 "f63e390479bde2e4d263250b045feb02db3fb5c118ffe6d0553f13967ec66c61"
 
-  url "https://github.com/MathieuDubart/cassette/releases/download/v#{version}/Cassette-#{version}.dmg",
-    verified: "github.com/MathieuDubart/cassette/"
+  url "https://github.com/CassetteLab/cassette/releases/download/v#{version}/Cassette-#{version}.dmg",
+      verified: "github.com/CassetteLab/cassette/"
   name "Cassette"
   desc "Subsonic music client"
   homepage "https://getcassette.app/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)*)\+\d+$/i)
+    strategy :github_latest
+  end
 
   depends_on macos: :sequoia
 
@@ -17,10 +23,4 @@ cask "cassette" do
     "~/Library/Caches/fr.mathieu-dubart.Cassette",
     "~/Library/Preferences/fr.mathieu-dubart.Cassette.plist",
   ]
-
-  livecheck do
-    url :url
-    regex(/^v?(\d+(?:\.\d+)*)\+\d+$/i)
-    strategy :github_latest
-  end
 end
